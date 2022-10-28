33e3c812265838f31dad43f386a8e90341932680
Toan Dao <daomanhtoan996t@gmail.com>
2022-10-28 21:34:24 +0700

    add-variable2class

--- org/o7planning/tutorial/javabasic/variable/VariableExample2.java
+++ org/o7planning/tutorial/javabasic/variable/VariableExample2.java
@@ -1,4 +1,15 @@
 package org.o7planning.tutorial.javabasic.variable;
 
 public class VariableExample2 {
+    public static void main(String[] args) {
+        long firstNumber, secondNumber, thirdNumber;
+
+        firstNumber = 100L;
+        secondNumber = 200L;
+        thirdNumber = firstNumber + secondNumber;
+        System.out.println("First Number = " + firstNumber);
+        System.out.println("Second Number = " + secondNumber);
+        System.out.println("Third Number = " + thirdNumber);
+    }
+
 }
