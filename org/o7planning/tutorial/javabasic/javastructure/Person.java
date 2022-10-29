4e9d7a526a34961403f05cdd04aa0e2b13409ffd
Toan Dao <daomanhtoan996t@gmail.com>
2022-10-29 14:00:24 +0700

    javastructure-class

---
+++ org/o7planning/tutorial/javabasic/javastructure/Person.java
@@ -0,0 +1,12 @@
+package org.o7planning.tutorial.javabasic.javastructure;
+
+public class Person {
+    public String name;
+    public Person(String personName) {
+        this.name = personName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ org/o7planning/tutorial/javabasic/javastructure/PersonTest.java
@@ -0,0 +1,15 @@
+package org.o7planning.tutorial.javabasic.javastructure;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person edison = new Person("Edison");
+
+        String name = edison.getName();
+        System.out.println("Person 1: " + name);
+
+        Person billGate = new Person("Bill Gates");
+
+        String name2 = billGate.name;
+        System.out.println("Person 2: " + name2);
+    }
+}
