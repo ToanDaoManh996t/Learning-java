25cf96d302813fe1c7bf49717575bc6768b22e6f
Toan Dao <daomanhtoan996t@gmail.com>
2022-10-29 15:10:33 +0700

    javastructure-FieldSample

---
+++ org/o7planning/tutorial/javabasic/javastructure/FieldSample.java
@@ -0,0 +1,9 @@
+package org.o7planning.tutorial.javabasic.javastructure;
+
+public class FieldSample {
+    public static int MY_STATIC_FIELD = 100;
+    public String myValue;
+    public FieldSample(String myValue) {
+        this.myValue = myValue;
+    }
+}
