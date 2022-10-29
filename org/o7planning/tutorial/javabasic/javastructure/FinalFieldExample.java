f28bb3b6a2f3dc47b1b0914a85d3e4b3ab1f4ce6
Toan Dao <daomanhtoan996t@gmail.com>
2022-10-29 18:22:15 +0700

    javastructure-FinalField

---
+++ org/o7planning/tutorial/javabasic/javastructure/FinalFieldExample.java
@@ -0,0 +1,9 @@
+package org.o7planning.tutorial.javabasic.javastructure;
+
+public class FinalFieldExample {
+    public static void main(String[] args) {
+        final int myValue = 100;
+        final long MY_LONG_VALUE = 1234L;
+        System.out.println(MY_LONG_VALUE);
+    }
+}
