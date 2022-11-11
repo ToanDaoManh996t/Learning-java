5549f70e07be30b0e5fcc092e3c927c96bf68278
Toan Dao <daomanhtoan996t@gmail.com>
2022-11-11 13:59:19 +0700

    javaenum-AbstractMethod

---
+++ org/o7planning/tutorial/javaenum/ColorDemo.java
@@ -0,0 +1,13 @@
+package org.o7planning.tutorial.javaenum;
+
+public class ColorDemo {
+    public static void main(String[] args) {
+        Color red = Color.RED;
+        Color blue = Color.BLUE;
+        Color green = Color.GREEN;
+
+        System.out.println(red.getHexCode());
+        System.out.println(blue.getHexCode());
+        System.out.println(green.getHexCode());
+    }
+}
