305c99698a83b1180bd641a246019b820c322a06
Toan Dao <daomanhtoan996t@gmail.com>
2022-11-11 14:41:36 +0700

    annotation-override

---
+++ org/o7planning/tutorial/ann/builtin/JavaCoding.java
@@ -0,0 +1,8 @@
+package org.o7planning.tutorial.ann.builtin;
+
+public class JavaCoding extends Job {
+    @Override
+    public String getName() {
+        return "Java Coding";
+    }
+}
---
+++ org/o7planning/tutorial/ann/builtin/Job.java
@@ -0,0 +1,7 @@
+package org.o7planning.tutorial.ann.builtin;
+
+public class Job {
+    public String getName() {
+        return null;
+    }
+}
